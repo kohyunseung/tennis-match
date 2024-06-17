7eefbacf996b76d378f794d83c8fb10a7133ea11
buzz <buzz.burn@kakaocorp.com>
2024-06-17 14:47:25 +0900

    fix: remove err

--- lib/file.ts
+++ lib/file.ts
@@ -2,7 +2,6 @@ import { readFile } from "fs/promises";
 
 export async function customReadFile(path: string) {
   try {
-    path.join(process.cwd(), path)
     const file = await readFile(process.cwd() + path, "utf8");
     return file;
   } catch (e) {
