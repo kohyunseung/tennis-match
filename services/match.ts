96feda110b1fe9047e9c38333be943af0f0b7640
hyunseung <hyun910625@naver.com>
2024-06-17 21:23:56 +0900

    fix: api url change

--- services/match.ts
+++ services/match.ts
@@ -1,11 +1,11 @@
 import { http } from "@/lib/http";
 
 export function fetchTodayMatches() {
-  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/today-match`);
+  return fetch(`api/today-match`);
 }
 
 export function postTodayMatches(data: any) {
-  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/today-match`, {
+  return fetch(`api/today-match`, {
     method: "POST",
     body: JSON.stringify(data),
   });
--- services/rank.ts
+++ services/rank.ts
@@ -1,5 +1,5 @@
 export function postPlayersRank(data: any) {
-  return fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/rank`, {
+  return fetch(`api/rank`, {
     method: "POST",
     body: JSON.stringify(data),
   });
