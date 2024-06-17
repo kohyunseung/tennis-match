1ae60086d461b040faa36685457eabdd78faa781
buzz <buzz.burn@kakaocorp.com>
2024-06-17 13:04:19 +0900

    test

--- services/match.ts
+++ services/match.ts
@@ -1,11 +1,11 @@
 import { http } from "@/lib/http";
 
 export function fetchTodayMatches() {
-  return fetch("http://localhost:3000/api/today-match");
+  return fetch("/api/today-match");
 }
 
 export function postTodayMatches(data: any) {
-  return fetch("http://localhost:3000/api/today-match", {
+  return fetch("/api/today-match", {
     method: "POST",
     body: JSON.stringify(data),
   });
